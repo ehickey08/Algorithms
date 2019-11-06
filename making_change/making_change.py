2935874b4b1a7777e6818b91c9bf86f344ea0bf9
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-11-06 16:57:36 -0500

    making change with a cache, works on large cases

--- making_change/making_change.py
+++ making_change/making_change.py
@@ -2,18 +2,30 @@
 
 import sys
 
+
+# naive implementation, works for small values
+# def making_change(amount, denominations):
+#     if amount <= 1:
+#         return 1
+#     count = 0
+#     for i, coin in enumerate(denominations):
+#         if amount - coin >= 0:
+#             count += making_change(amount - coin, denominations[:i+1])
+#
+#     return count
+
+# use the hint from repo for caching
+
 def making_change(amount, denominations):
-    if amount <= 1:
-        return 1
-    count = 0
-    for i, coin in enumerate(denominations):
-        if amount - coin >= 0:
-            count += making_change(amount - coin, denominations[:i+1])
+    cache = [1] + [0] * amount
 
-    return count
+    for coin in denominations:
+        for value in range(coin, amount + 1):
+            cache[value] += cache[value - coin]
+
+    return cache[amount]
 
 
-print(making_change(22, [1,5,10,25,50]))
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python making_change.py [amount]` with different amounts
