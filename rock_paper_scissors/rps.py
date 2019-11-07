fc9407207d13b62f339cf552d1a21beb3169b745
ehickey08 <49757261+ehickey08@users.noreply.github.com>
2019-11-07 10:29:21 -0500

    tried another rps, slower than the first

--- rock_paper_scissors/rps.py
+++ rock_paper_scissors/rps.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 
 import sys
-
+import time
 
 def rock_paper_scissors(n):
     def add_move(moves, move):
@@ -18,8 +18,25 @@ def rock_paper_scissors(n):
     add_move(moves, [])
     return moves
 
-print(rock_paper_scissors(2))
-print(rock_paper_scissors(5))
+def rock_paper_scissors_slower(n):
+    def add_move(moves):
+        multiplied_moves = [move for move in moves for i in range(3)]
+        for i, move in enumerate(multiplied_moves):
+            if i % 3 == 0:
+                multiplied_moves[i] = move + ['rock']
+            if i % 3 == 1:
+                multiplied_moves[i] = move + ['paper']
+            if i % 3 == 2:
+                multiplied_moves[i] = move + ['scissors']
+        return multiplied_moves
+
+    if n == 0:
+        return [[]]
+    elif n == 1:
+        return [['rock'], ['paper'], ['scissors']]
+    else:
+        return add_move(rock_paper_scissors_slower(n-1))
+
 if __name__ == "__main__":
     if len(sys.argv) > 1:
         num_plays = int(sys.argv[1])
